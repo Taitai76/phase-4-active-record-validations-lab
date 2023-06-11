94d4257430d584cca24f0332e721ae89b4d398fd
Mohammed Taitai <mohammedtaitai@outlook.com>
2023-06-11 14:37:52 -0400

    commit tree c030807597afdee86c86b26a68a5a67ee33a873a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', "Non-Fiction"]}
+    validate :sufficiently_clickbaity
+
+    private 
+    def sufficiently_clickbaity
+        unless title.to_s.include?("Won't Believe")
+            errors.add(:title, 'Not clickbait-y')
+        end
+    end
+    
 end
